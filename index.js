cc289c1e989e7badf251335c4aae53bc6f44bd5d
fxfuren <nikitostimoshenko@gmail.com>
2024-01-23 23:21:47 +0300

    add some func backend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(
   express.urlencoded(),
   cors({
-    origin: 'http://localhost:8080'
+    origin: 'http://localhost:5173'
   })
 );
 
@@ -21,4 +21,49 @@ const client = new Client({
     port: process.env.DB_PORT
 });
 
+
+app.get("/loadClientsData", (req, res) => {
+    client.query('SELECT * FROM Clients', (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const clients = result.rows;
+        res.send(clients);
+      }
+    });
+  });
+  
+  app.post("/addClient", (req, res) => {
+    const { FioClient, PhoneNumber, Mail, TypeClientID } = req.body;
+  
+    const query = 'INSERT INTO clients (FioClient, PhoneNumber, Mail, TypeClientID) VALUES ($1, $2, $3, $4) RETURNING *';
+    const values = [FioClient, PhoneNumber, Mail, TypeClientID];
+  
+    client.query(query, values, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const newClient = result.rows[0];
+        res.send(newClient);
+      }
+    });
+  });
+
+  app.get("/loadClientTypes", (req, res) => {
+    client.query('SELECT TypeClientID, NameType FROM TypeClients', (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const clientTypes = result.rows;
+        res.send(clientTypes);
+      }
+    });
+  });
+
 client.connect(() => console.log('DB is connected'));
+app.listen(3300, () => {
+    console.log("Server started on port 3300");
+  });
