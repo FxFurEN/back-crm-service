896f637aae11157a815db624fced1b47306e399b
fxfuren <nikitostimoshenko@gmail.com>
2024-01-28 17:28:13 +0300

    add user-controller

--- routes/crmRoutes.js
+++ routes/crmRoutes.js
@@ -1,11 +1,19 @@
 const { Router } = require('express');
 const crmController = require('../controllers/crmController');
 const clientsController = require('../controllers/clients-сontroller');
+const userController = require('../controllers/user-controller');
 const router = new Router();
+
+
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/refresh', userController.refresh);
 
 router.get('/loadClientsData', clientsController.loadClientData);
 router.post('/addClient', clientsController.addClient);
 router.get('/loadClientTypes', crmController.loadClientTypes);
+
 router.get('/loadCategories', crmController.loadCategories);
 router.post('/addCategory', crmController.addCategory);
 router.get('/loadGoods', crmController.loadGoods);
