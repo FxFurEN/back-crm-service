d961536e7fe1960b125bc5c3e43602d3b608a427
FxFurEN <nikitostimoshenko@gmail.com>
2024-03-06 08:36:15 +0300

    push to dev

---
+++ controllers/crmController.js
@@ -0,0 +1,72 @@
+const client = require('../db');
+const loadClientTypes = (req, res) => {
+  client.query('SELECT TypeClientID, NameType FROM TypeClients', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const clientTypes = result.rows;
+      res.send(clientTypes);
+    }
+  });
+};
+
+const loadCategories = (req, res) => {
+    client.query('SELECT namecategory FROM CategoryGoods', (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          const categories = result.rows;
+          res.send(categories);
+        }
+      });
+};
+
+const addCategory = (req, res) => {
+    const { name } = req.body;
+    client.query('INSERT INTO CategoryGoods (NameCategory) VALUES ($1) RETURNING *', [name], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        const newCategory = result.rows[0];
+        res.send(newCategory);
+      }
+    });
+};
+
+const loadGoods = (req, res) => {
+    client.query('SELECT namegood, retailprice, purchaseprice, article, amount, category  FROM GoodsView', (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          const goods = result.rows;
+          res.send(goods);
+        }
+      });
+};
+
+const addGoods = (req, res) => {
+    const { article, category, name, amount, price, costPrice } = req.body;
+    client.query('INSERT INTO Goods (Article, NameGood, Amount, RetailPrice, PurchasePrice, CategoryId) VALUES ($1, $2, $3, $4, $5, (SELECT CategoryID FROM CategoryGoods WHERE NameCategory = $6)) RETURNING *',
+      [article, name, amount, price, costPrice, category],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          const newGoods = result.rows[0];
+          res.send(newGoods);
+        }
+      });
+};
+
+module.exports = {
+  loadClientTypes,
+  loadCategories,
+  addCategory,
+  loadGoods,
+  addGoods,
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const client = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+const crmRoutes = require('./routes/crmRoutes');
+
+app.use('/crm', crmRoutes);
+
+client.connect(() => console.log('DB is connected'));
+app.listen(3300, () => {
+    console.log("Server started on port 3300");
+});
